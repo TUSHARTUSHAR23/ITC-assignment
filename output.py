3863db0a73bce6c2bff5ca832cbdfcec75d13d9c
TUSHARTUSHAR23 <107211946+TUSHARTUSHAR23@users.noreply.github.com>
2022-06-18 09:47:42 -0700

    Add files via upload

---
+++ output.py
@@ -0,0 +1,76 @@
+ANS.1
+write the string:tushar
+original string: tushar
+reversed string is: rahsut 
+ANS.2
+Enter the minimum value :5
+Enter the maximum value: 67
+Enter the number for divisibility check :7
+7
+14
+21
+28
+35
+42
+49
+56
+63
+ANS.3
+Enter the length of side:56
+Enter the length of side:78
+Enter the length of side:98
+2181.889089756856
+ANS.4
+Enter the no. of rows :6
+*
+**
+***
+****
+***
+**
+*
+ANS.5
+Enter the number of rows7
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYZAB
+ANS.6
+Enter the Lowest Range Value : 5
+Enter the Upper Range Value : 12
+The Prime Numbers in this range are : 
+5
+7
+11
+ANS.7
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+ANS.8
+Enter the number 56
+Enter the number 67
+Enter the number 64
+Enter the number 43
+Enter the number -67
+Enter the number -90
+Enter the number 76
+Enter the number 54
+Enter the number 423
+Enter the number 44
+Positive numbers are:  [56, 67, 64, 43, 76, 54, 423, 44]
+Negative numbers are:  [-67, -90]
+Even numbers are:  [56, 64, -90, 76, 54, 44]
+Odd numbers are:  [67, 43, -67, 423]
+Number of times each number occurs in the List {56: 1, 67: 1, 64: 1, 43: 1, -67: 1, -90: 1, 76: 1, 54: 1, 423: 1, 44: 1}
+ANS.9
+Number of words: 7
+Enter the word: khokhar
+Enter the word: kohli
+Enter the word: rohit
+Enter the word: jashan
+Enter the word: manav
+Enter the word: goyal
+Enter the word: tushar
+Number of occurences:  {'khokhar': 1, 'kohli': 1, 'rohit': 1, 'jashan': 1, 'manav': 1, 'goyal': 1, 'tushar': 1}
